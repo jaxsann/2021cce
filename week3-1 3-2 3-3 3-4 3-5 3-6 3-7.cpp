5ba4977aba41cf8bc5f7daac5ae9ca975f8fe47e
wu <91170827+jaxsann@users.noreply.github.com>
2021-09-29 12:12:58 +0800

    Add files via upload

---
+++ week3-1 3-2 3-3 3-4 3-5 3-6 3-7.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int main()
+{
+    printf( "Hello World");
+}
+{
+    printf( "Hello World\n");
+    printf( "Hello World\n");
+}
+{
+    int a, b;
+    scanf( "%d%d", &a, &b );
+    printf( "%d", a+b )
+}
+{
+    printf( "Hello World\n");
+    printf( "%d" , 300 );
+}
+{
+    printf( "Hello World\n");
+    printf( "Hello%dWorld\n", 300);
+    printf( "%d %d    %d" , 300,200,100 );
+}
+{
+    int a;
+    printf("請輸入一個整數a的值: ");
+    scanf("%d", &a);
+
+    printf("a的值是:%d\n" , a);
+}
+
+    #include <stdio.h>
+int main()
+{
+	int a, b;
+	scanf("%d%d", &a, &b );
+	printf("%d", a+b );
+}
+
+
