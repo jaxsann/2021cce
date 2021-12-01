c6a8d5091db477c4f8fa54fbae40328cc1672629
wu <91170827+jaxsann@users.noreply.github.com>
2021-12-01 12:01:44 +0800

    Add files via upload

---
+++ week12/week12-3.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int a[10]{3,0,1,8,7,2,5,4,6,9};
+int main()
+{
+    for(int i=0;i<10;i++)printf("%d " ,a[i]);
+    printf("\n");
+
+        for(int i=0;i<10;i++){///左手i
+            for(int j=i+1;j<10;j++){///右手j (i的右邊開始巡)
+                ///左手i,右手j(希望小...大)
+            if(a[i]>a[j]){///如果反過來
+                int temp=a[i];///交換
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+        }
+        for(int i=0;i<10;i++)printf("%d ",a[i]);
+        printf("\n");///印出陣列
+
+}
